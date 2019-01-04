50149c9f57c75ca096f7286c3283bcb60ccf9b7a
nohosh <sanchit8b@gmail.com>
2019-01-04 06:42:27 +0530

    started impelmenting layout and burger builder src

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,9 +1,11 @@
 import React from 'react';
 
+import classes from './Layout.css';
+
 const layout = ( props ) => (
     <>
         <div> Tooolbar, Sidedrawer,Backdrop</div>
-        <main>
+        <main className={classes.Content}>
             {props.children}
         </main>
     </>
