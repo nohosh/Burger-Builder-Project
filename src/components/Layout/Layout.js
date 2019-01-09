19292d8f4ec4652cdba0e389d76764244cdbc537
nohosh <sanchit8b@gmail.com>
2019-01-09 22:20:10 +0530

    toolbar added to UI

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,10 +1,12 @@
 import React from 'react';
 
+import Toolbar from '../Navigation/Toolbar/Toolbar';
+
 import classes from './Layout.css';
 
 const layout = ( props ) => (
     <>
-        <div> Tooolbar, Sidedrawer,Backdrop</div>
+        <Toolbar/>
         <main className={classes.Content}>
            {props.children}
         </main>
---
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import classes from './Toolbar.css';
+
+const toolbar = (props) => (
+    <header className={classes.Toolbar}>
+        <div>MENU</div>
+        <div>LOGO </div>
+        <nav>
+            ...{/* list will go here */}
+        </nav>
+    </header>
+);
+
+export default toolbar;
