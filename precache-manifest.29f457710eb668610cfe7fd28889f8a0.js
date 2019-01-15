078990bc24c18979aeb4fac955737c55d0ddaa4d
nohosh <sanchit8b@gmail.com>
2019-01-15 23:36:27 +0530

    Updates

---
+++ precache-manifest.29f457710eb668610cfe7fd28889f8a0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/Burger-Builder-Project/static/media/burger-logo.b8503d26.png"
+  },
+  {
+    "revision": "c473ac56083ebebcee78",
+    "url": "/Burger-Builder-Project/static/js/runtime~main.c473ac56.js"
+  },
+  {
+    "revision": "3d4e5825a77dd3bfc6c4",
+    "url": "/Burger-Builder-Project/static/js/main.3d4e5825.chunk.js"
+  },
+  {
+    "revision": "68c493a9d8bb3e6415d4",
+    "url": "/Burger-Builder-Project/static/js/1.68c493a9.chunk.js"
+  },
+  {
+    "revision": "3d4e5825a77dd3bfc6c4",
+    "url": "/Burger-Builder-Project/static/css/main.072026df.chunk.css"
+  },
+  {
+    "revision": "08cce40ec7b355b3a84bde15cbd04559",
+    "url": "/Burger-Builder-Project/index.html"
+  }
+];
